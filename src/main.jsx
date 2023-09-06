a7891d2b5e7a83093ead4a8711024c05d3af6241
Kacper <kacpercybulski@icloud.com>
2023-09-06 21:53:55 +0200

    everything is ready for a project

--- src/main.jsx
+++ src/main.jsx
@@ -3,4 +3,8 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
-// pobrac za pomoca rout'a 
+ReactDOM.createRoot(document.getElementById('root')).render(
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>
+)
