f3a1d8faf84d1321b15548a132b05709f8f5c209
Kacper <kacpercybulski@icloud.com>
2023-09-12 13:45:09 +0200

    navbar is working

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import Hero from './components/Hero'
 import Nav from './components/Nav'
 import './App.css'
 
--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: "/AirBnb-clone/"
+	plugins: [react()],
+	server: {
+		port: 4000,
+	},
 })
